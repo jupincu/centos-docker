796269e3ce53c6f568cfa30b094fb19db89bab09
nopost <nopost@qq.com>
2019-10-09 09:10:32 +0800

    add centos-sshd

---
+++ centos-sshd/container-files/install/build/0002_yum_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+yum install -y \
+    iproute \
+    python-setuptools \
+    hostname \
+    inotify-tools \
+    yum-utils \
+    which \
+    wget \
+    net-tool \
+    jq \
+    openssl \
+    openssh-server \
+    openssh-clients \
+    rsync \
+    initscripts \
+    telnet  
