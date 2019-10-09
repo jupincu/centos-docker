8f597ba7a733a6fc12256ef804863fe3b2fb0fa5
nopost <nopost@qq.com>
2019-10-09 13:32:23 +0800

    lap

--- centos-baota/container-files/install/build/0001_install_baota.sh
+++ centos-baota/container-files/install/build/0001_install_baota.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-cd ~ && \
-wget -O install.sh http://download.bt.cn/install/install_6.0.sh  && \
-echo y | bash install.sh && \
-python /config/set_bt_default.py && \
-bt 10 && \
-bt 11 && \
-bt 12 && \
-bt 13 && \
-bt 14 && \
-bt 1 && \
-yum clean all
+cd /www/server/panel \
+    && bash ./install/install_soft.sh 0 install nginx 1.17 \
+    && bash ./install/install_soft.sh 0 install php 7.3 \
+    && bash ./install/install_soft.sh 0 install php 7.2 \
+    && bash ./install/install_soft.sh 0 install php 7.0 \
+    && bash ./install/install_soft.sh 0 install php 5.6 \
+    && bash ./install/install_soft.sh 0 install php 5.3 \
+    && echo '["linuxsys", "nginx", "php-7.3"，"php-7.2"，"php-7.0"，"php-5.6"，"php-5.3"]' > ./config/index.json   
+
+VOLUME ["/www","/www/wwwroot"]
