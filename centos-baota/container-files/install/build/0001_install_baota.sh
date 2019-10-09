0e26b4d70e08311d436e9b40da5e74156b05aebe
nopost <nopost@qq.com>
2019-10-09 17:11:26 +0800

    10
    10

--- centos-baota/container-files/install/build/0001_install_baota.sh
+++ centos-baota/container-files/install/build/0001_install_baota.sh
@@ -6,6 +6,6 @@ cd /www/server/panel \
     && bash ./install/install_soft.sh 0 install php 7.0 \
     && bash ./install/install_soft.sh 0 install php 5.6 \
     && bash ./install/install_soft.sh 0 install php 5.3 \
+    && bash ./install/install_soft.sh 0 install redis 5.0.5 \
     && echo '["linuxsys", "nginx", "php-7.3"，"php-7.2"，"php-7.0"，"php-5.6"，"php-5.3"]' > ./config/index.json   
-
 VOLUME ["/www","/www/wwwroot"]
